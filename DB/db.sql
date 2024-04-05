6a04f51a7e41c35da3437c02e3307bc6b781ed8c
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-05 19:07:54 +0200

    DB updated

--- DB/db.sql
+++ DB/db.sql
@@ -36,6 +36,15 @@ CREATE TABLE accessories (
 
 
 -- medicals table
+CREATE TABLE medicals (
+    medical_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(255),
+    price DECIMAL(10, 2)
+);
+
+
+-- appointments table
 
 
 -- orders table
