99ec670bdd31327d92f8129dc3a3a5877b63d1ca
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-05 19:03:50 +0200

    DB updated

--- DB/db.sql
+++ DB/db.sql
@@ -40,9 +40,13 @@ CREATE TABLE orders (
 
 -- order_items table
 CREATE TABLE order_items (
-    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_item_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
-    pet_id INT NOT NULL,
+    item_id INT NOT NULL,
     item_type ENUM('pet', 'accessory') NOT NULL,
     quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (item_id) REFERENCES pets(pet_id),
+    FOREIGN KEY (item_id) REFERENCES accessories(accessory_id)
 
