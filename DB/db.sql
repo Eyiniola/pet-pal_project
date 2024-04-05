aadea257bd4278b9457e67d5ff545d4055442d2b
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-05 18:57:55 +0200

    DB created

--- DB/db.sql
+++ DB/db.sql
@@ -4,6 +4,8 @@ CREATE DATABASE pet_pal_db;
 
 USE pet_pal_db;
 
+
+-- users table
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) UNIQUE NOT NULL,
@@ -11,4 +13,15 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE NOT NULL,
     full_name VARCHAR(100) NOT NULL,
     phone_number VARCHAR(20) NOT NULL
+);
+
+-- pets table
+CREATE TABLE pets (
+    pet_id INT AUTO_INCREMENT PRIMARY KEY,
+    pet_name VARCHAR(100) NOT NULL,
+    species VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    descriptions TEXT NOT NULL,
+    image_url VARCHAR(255)
 );
