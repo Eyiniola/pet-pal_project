1801e2ff6ca17d4604244fd2f28d7f0a34d0bcbd
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-05 19:01:00 +0200

    DB updated

--- DB/db.sql
+++ DB/db.sql
@@ -25,3 +25,24 @@ CREATE TABLE pets (
     descriptions TEXT NOT NULL,
     image_url VARCHAR(255)
 );
+
+
+-- orders table
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    payment_status VARCHAR(20) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+
+-- order_items table
+CREATE TABLE order_items (
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    pet_id INT NOT NULL,
+    item_type ENUM('pet', 'accessory') NOT NULL,
+    quantity INT NOT NULL,
+
