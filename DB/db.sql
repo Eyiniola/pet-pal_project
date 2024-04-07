0d078e56cf2df2a0bce5b0f8cc2ea3aac7dd0335
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-07 14:11:48 +0200

    updated

--- DB/db.sql
+++ DB/db.sql
@@ -3,8 +3,6 @@
 CREATE DATABASE pet_pal_db;
 
 USE pet_pal_db;
-
-
 -- users table
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -14,7 +12,6 @@ CREATE TABLE users (
     full_name VARCHAR(100) NOT NULL,
     phone_number VARCHAR(20) NOT NULL
 );
-
 -- pets table
 CREATE TABLE pets (
     pet_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -24,7 +21,6 @@ CREATE TABLE pets (
     price DECIMAL(10, 2) NOT NULL,
     image_url VARCHAR(255)
 );
-
 -- accessories table
 CREATE TABLE accessories (
     accessory_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -33,8 +29,6 @@ CREATE TABLE accessories (
     price DECIMAL(10, 2),
     image_url VARCHAR(255)
 );
-
-
 -- medicals table
 CREATE TABLE medicals (
     medical_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -42,8 +36,6 @@ CREATE TABLE medicals (
     description VARCHAR(255),
     price DECIMAL(10, 2)
 );
-
-
 -- appointments table
 CREATE TABLE appointments (
     appointment_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -53,8 +45,6 @@ CREATE TABLE appointments (
     notes TEXT,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
-
 -- immunizations table
 CREATE TABLE immunizations (
     immunization_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -64,8 +54,6 @@ CREATE TABLE immunizations (
     next_due_date DATE,
     FOREIGN KEY (pet_id) REFERENCES pets(pet_id)
 );
-
-
 -- reviews table
 CREATE TABLE reviews (
     review_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -76,8 +64,6 @@ CREATE TABLE reviews (
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (pet_id) REFERENCES pets(pet_id)
 );
-
-
 -- cart table
 CREATE TABLE carts (
     cart_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -85,8 +71,6 @@ CREATE TABLE carts (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
-
 -- cart_items table
 CREATE TABLE cart_items (
     cart_item_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -99,8 +83,6 @@ CREATE TABLE cart_items (
     FOREIGN KEY (product_id) REFERENCES pets(pet_id),
     FOREIGN KEY (product_id) REFERENCES accessories(accessory_id)
 );
-
-
 -- orders table
 CREATE TABLE orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -110,8 +92,6 @@ CREATE TABLE orders (
     payment_status VARCHAR(20) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
-
 -- order_items table
 CREATE TABLE order_items (
     order_item_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -123,4 +103,4 @@ CREATE TABLE order_items (
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (product_id) REFERENCES pets(pet_id),
     FOREIGN KEY (product_id) REFERENCES accessories(accessory_id)
-
+);
