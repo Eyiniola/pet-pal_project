4b478f62dd990eeeaa1c2e718219e99b67ba91f0
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-07 14:56:14 +0200

    updated

--- backend/models/accesories.py
+++ backend/models/accesories.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from services.database import db
 
-Base = declarative_base()
 
-class accesory(Base):
-    __tablename__ = 'accesory'
+class Accessories(db.Base):
+    __tablename__ = 'accessories'
+
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    type = Column(String)
-    description = Column(String)
-    image = Column(String)
+    name = Column(String(50))
+    price = Column(Integer)
+    image = Column(String(100))
 
     def __repr__(self):
-        return "<accesory(id={self.id}, name={self.name}, type={self.type}, description={self.description}, image={self.image})>"
+        return "<Accessories(id={self.id}, name={self.name})>"
     
+class ShoppingCart(db.Base):
+    __tablename__ = 'shopping_cart'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    pet_id = Column(Integer, ForeignKey('pets.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    item_id = Column(Integer)
+    quantity = Column(Integer)
+    price = Column(Integer)
+    pet = relationship('Pets', back_populates='shopping_cart')
+    user = relationship('Users', back_populates='shopping_cart')
+
+    def __repr__(self):
+        return "<ShoppingCart(id={self.id}, name={self.name})>"
