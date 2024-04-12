770aed81c1a1bc860dc31514a810e30ab9da79c6
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-12 14:03:36 +0200

    updated

--- backend/app.py
+++ backend/app.py
@@ -6,13 +6,17 @@ from controllers.accesoryController import accesory
 from controllers.medicalController import medical
 from controllers.adoptController import adopt
 from services.database import db
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
 #  configure the database
 app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql://petPalAdmin:petPalAdmin1244@localhost/pet_pal_project'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+
+db = SQLAlchemy(app)
+# db.init_app(app)
 
 #  register the blueprints
 app.register_blueprint(accesory)
